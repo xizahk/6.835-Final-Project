5c1b200e6ef1df978e03770ee4b12fae7e2bd2ab
xizahk <ywang0598@gmail.com>
2020-04-19 15:09:23 -0400

    Add files for maintaining game state and constants

---
+++ Assets/GameScripts/Constants.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Constants : MonoBehaviour
+{
+   // Add new constants to this script as needed, e.g.
+   // public const int NUMBER_OF_PLAYERS = 4;
+}
---
+++ Assets/GameScripts/Game.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    // Main script to maintain game state logic, e.g:
+    // public enum gameState {
+    //      menu,
+    //      ongoing,
+    //      gameover
+    // }
+    // 
+    // private gameState state = gameState.menu;
+    // 
+    // <functions that return current game state / progress to next game state>
+}
